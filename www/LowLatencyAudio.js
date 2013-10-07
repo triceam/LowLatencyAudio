736e351f19cc907e8b6208c49996208c838dd1ba
Andrew Trice <triceam@Andrew-Trices-MacBook-Pro.local>
2013-10-07 09:54:41 -0400

    updated new js structure

--- www/LowLatencyAudio.js
+++ www/LowLatencyAudio.js
@@ -1,40 +1,35 @@
-var LowLatencyAudio = {
+cordova.define("com.phonegap.LowLatencyAudio", function(require, exports, module) {
+
+	var exec = require('cordova/exec');
+
+	var LowLatencyAudio = {
   
-preloadFX: function ( id, assetPath, success, fail) {
-    if ( cordova.exec ) {
-        return cordova.exec(success, fail, "com.phonegap.LowLatencyAudio", "preloadFX", [id, assetPath]);
-    }
-},    
-    
-preloadAudio: function ( id, assetPath, voices, success, fail) {
-    if ( cordova.exec ) {
-        return cordova.exec(success, fail, "com.phonegap.LowLatencyAudio", "preloadAudio", [id, assetPath, voices]);
-    }
-},
-    
-play: function (id, success, fail) {
-    if ( cordova.exec ) {
-        return cordova.exec(success, fail, "com.phonegap.LowLatencyAudio", "play", [id]);
-    }
-},
-    
-stop: function (id, success, fail) {
-    if ( cordova.exec ) {
-        return cordova.exec(success, fail, "com.phonegap.LowLatencyAudio", "stop", [id]);
-    }
-},
-    
-loop: function (id, success, fail) {
-    if ( cordova.exec ) {
-        return cordova.exec(success, fail, "com.phonegap.LowLatencyAudio", "loop", [id]);
-    }
-},
-    
-unload: function (id, success, fail) {
-    if ( cordova.exec ) {
-        return cordova.exec(success, fail, "com.phonegap.LowLatencyAudio", "unload", [id]);
-    }
-}
-    
-    
-};
+		preloadFX: function ( id, assetPath, success, fail) {
+			return exec(success, fail, "com.phonegap.LowLatencyAudio", "preloadFX", [id, assetPath]);
+		},    
+	
+		preloadAudio: function ( id, assetPath, voices, success, fail) {
+			return cordova.exec(success, fail, "com.phonegap.LowLatencyAudio", "preloadAudio", [id, assetPath, voices]);
+		},
+	
+		play: function (id, success, fail) {
+			return cordova.exec(success, fail, "com.phonegap.LowLatencyAudio", "play", [id]);
+		},
+	
+		stop: function (id, success, fail) {
+			return cordova.exec(success, fail, "com.phonegap.LowLatencyAudio", "stop", [id]);
+		},
+	
+		loop: function (id, success, fail) {
+			return cordova.exec(success, fail, "com.phonegap.LowLatencyAudio", "loop", [id]);
+		},
+	
+		unload: function (id, success, fail) {
+			return cordova.exec(success, fail, "com.phonegap.LowLatencyAudio", "unload", [id]);
+		}
+	};
+
+	module.exports = new LowLatencyAudio();
+});
+
+
