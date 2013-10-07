1485ce6891ae31866b6a6f94dd817d2dbc5a8657
Andrew Trice <triceam@Andrew-Trices-MacBook-Pro.local>
2013-10-07 12:25:16 -0400

    fixing the exec references i forgot to copy over...

--- www/LowLatencyAudio.js
+++ www/LowLatencyAudio.js
@@ -9,23 +9,23 @@ var LowLatencyAudio = {
 	},    
 
 	preloadAudio: function ( id, assetPath, voices, success, fail) {
-		return cordova.exec(success, fail, "LowLatencyAudio", "preloadAudio", [id, assetPath, voices]);
+		return exec(success, fail, "LowLatencyAudio", "preloadAudio", [id, assetPath, voices]);
 	},
 
 	play: function (id, success, fail) {
-		return cordova.exec(success, fail, "LowLatencyAudio", "play", [id]);
+		return exec(success, fail, "LowLatencyAudio", "play", [id]);
 	},
 
 	stop: function (id, success, fail) {
-		return cordova.exec(success, fail, "LowLatencyAudio", "stop", [id]);
+		return exec(success, fail, "LowLatencyAudio", "stop", [id]);
 	},
 
 	loop: function (id, success, fail) {
-		return cordova.exec(success, fail, "LowLatencyAudio", "loop", [id]);
+		return exec(success, fail, "LowLatencyAudio", "loop", [id]);
 	},
 
 	unload: function (id, success, fail) {
-		return cordova.exec(success, fail, "LowLatencyAudio", "unload", [id]);
+		return exec(success, fail, "LowLatencyAudio", "unload", [id]);
 	}
 };
 
